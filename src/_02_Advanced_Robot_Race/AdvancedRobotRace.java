bebe0bc0a1e0afdc7323c1f40c9aa2fefb6840bc
league <league@imac-24.attlocal.net>
2019-11-10 11:29:26 -0800

    Finished RobotRace, Reverse Greeting

--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,62 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
 	// Re-do the robot race recipe from level 3 module 0. 
+	public static boolean end = false;
+	public static int winner = 0;
 	// This time, use threads to make all of the robots go at the same time.
+	public static void main(String[] args) {
+		int length = 500;
+		Robot[] robots = new Robot[length];
+		int i = 50;
+		int increaser = 1000/length;
+		for(int j = 0;j<robots.length;j++) {
+			robots[j] = new Robot();
+			robots[j].setX(i+=increaser);
+			robots[j].setY(800);
+		}
+		Thread[] threads = new Thread[length];
+		for(i = 0;i<robots.length;i++) {
+		int k = i;
+		threads[i] = new Thread(()->new AdvancedRobotRace().robotRun(robots[k],50,k));
+		}
+		for(Thread t: threads) {
+			t.start();
+		}
+		
+		
+	}
+	
+	public void robotRun(Robot rob,int condition, int number) {
+		Random randy = new Random();
+		rob.setSpeed(100);
+		while(rob.getY()>condition&&!end) {
+			rob.move(randy.nextInt(50));
+			
+		}
+		
+		winner = number;
+		if(!end) {
+		end = true;
+		System.out.println(winner);
+		System.out.println("end");
+		rob.turn(720);
+		JOptionPane.showMessageDialog(null, "Robot "+"#"+(winner+1)+" won!");
+		rob.turn(720);
+		}
+		
+		
+		
+		
+			
+			
+		
+	}
+
 }
--- src/_03_Threaded_Reverse_Greeting/ThreadedReverseGreeting.java
+++ src/_03_Threaded_Reverse_Greeting/ThreadedReverseGreeting.java
@@ -5,6 +5,24 @@ public class ThreadedReverseGreeting {
   //Thread 2 creates Thread 3; and so on, up to Thread 50. 
   //Each thread should print "Hello from Thread <num>!", 
   //but you should structure your program such that the threads print their greetings in reverse order.
-	
+	public static void main(String[] args) {
+		threadMaker(0,new Thread());
+	}
 	/* HINT: You will most likely need to do this with recursion */
+	public static void threadMaker(int number,Thread thread) {
+		if(number<=50) {
+		thread = new Thread(()->threadMaker(number+1,new Thread()));
+		thread.start();
+		try {
+			thread.join();
+			System.out.println(number);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
+		
+	}
+	
+	
 }
